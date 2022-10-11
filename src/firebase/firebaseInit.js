5fd9ad6fbbc147083ba53671191a3557f0858f23
Rufus Drew <64923036+RufusDrew@users.noreply.github.com>
2022-10-11 14:54:48 +0530

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCiC2C9os8y041_3Hx0oxgMTOP8U-T0RiY",
-    authDomain: "therosarians-b21de.firebaseapp.com",
-    projectId: "therosarians-b21de",
-    storageBucket: "therosarians-b21de.appspot.com",
-    messagingSenderId: "1051151607866",
-    appId: "1:1051151607866:web:b78b77dc1aaf7ec2b6ed03"
+    apiKey: "API-KEY",
+    authDomain: "DOMAIN-NAME",
+    projectId: "ID",
+    storageBucket: "SB-NAME",
+    messagingSenderId: "MS-SENDER-ID",
+    appId: "APP-ID"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
